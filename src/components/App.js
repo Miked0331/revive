bd5cdc737945acf4eba4d7aba415474bbb115937
miked0331 <mikedragun@gmail.com>
2019-06-07 09:32:20 -0400

    Start of it all

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Card from './Card'
+import './App.css'
+
+
+
+const App = () => {
+    return (
+        <div className="col-main tc grow ">
+            <div className="col-1 bb bt bl br">
+            <img src="image/p1.jpg" />
+            </div>
+            <div className="col-2 bb bt bl br">
+           hello
+            </div>
+            <div className="col-3 bb bt bl br">
+           hello
+            </div>
+         
+
+
+            </div>
+    )
+}
+
+export default App
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+    return(
+        <div>
+            <img src="image/p1.jpg" />
+        </div>
+       
+    )
+}
+
+export default Card
