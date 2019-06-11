e6d86d28c8d7238f9323b5e4c5deea429eaa96fe
Miked0331 <40614967+Miked0331@users.noreply.github.com>
2019-06-10 22:04:11 -0400

    new grid responsiveness

--- src/components/Info.js
+++ src/components/Info.js
@@ -25,17 +25,30 @@ export const Info = [
         desc: 'Frappucino',
         price: 'L- $4.25 M- $3.50'
     },
-    /*
+
     {
         id: 5,
-        img: <img src={p1} />,
-        desc: 'it is mee',
-        price: '2.50'
+        img: "https://images.unsplash.com/photo-1556742526-795a8eac090e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80/316x316",
+        desc: 'Caffe Latte',
+        price: 'L- $4.50 M- $3.75'
     },
     {
         id: 6,
-        img: <img src={p1} />,
-        desc: 'it is meeee',
-        price: '2.50'
-    },*/
+        img: "https://images.unsplash.com/photo-1514432324607-a09d9b4aefdd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80/316x316",
+        desc: "'famous' Americano",
+        price: 'L- $3.50 M- $2.75'
+    },
+    {
+        id: 7,
+        img: "https://images.unsplash.com/photo-1525480122447-64809d765ec4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80/316x316",
+        desc: 'Iced Coffe',
+        price: 'L- $3.50 M- $2.99'
+    },
+
+    {
+        id: 8,
+        img: "https://images.unsplash.com/photo-1509481167695-0f2353142dd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80/316x316",
+        desc: 'Frappucino',
+        price: 'L- $4.25 M- $3.50'
+    },
 ]
