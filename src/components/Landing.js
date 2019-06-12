1f8839a26dda4f4c43614fe810787f718bade302
Miked0331 <40614967+Miked0331@users.noreply.github.com>
2019-06-12 18:24:59 -0400

    new landing img

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
-import landing from './img/landing.jpg'
 
 const Landing = () => {
     return (
         <div className="landing">
-            <img src={landing} alt="coffee-logo" />
+            <div><img src="https://images.unsplash.com/photo-1490111718993-d98654ce6cf7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80/316x316" alt="coffee-logo" />
+            </div>
+            <div className="tc yellow">
+                "Coffee That Matters"
+            </div>
         </div>
     )
 }
