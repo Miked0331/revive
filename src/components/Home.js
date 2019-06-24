2e64239429cea45b5036960e8388ab78c7994360
Miked0331 <40614967+Miked0331@users.noreply.github.com>
2019-06-24 17:12:53 -0400

    new landing logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,6 @@ import React from 'react'
 import './App.css'
 import { Link } from 'react-router-dom'
 
-const color = {
-    color: 'red'
-}
-
 const Home = () => {
     return (
         <div className="home" >
@@ -15,7 +11,7 @@ const Home = () => {
                 </Link> for some deliciousness</p>
             </div>
             <div className='home-item'>
-                <img src="https://images.unsplash.com/photo-1529417305485-480f579e7578?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1409&q=80"></img>
+                <img src="https://images.unsplash.com/photo-1529417305485-480f579e7578?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1409&q=80" alt="coffe shop"></img>
             </div>
 
         </div>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './App.css'
+import revivelogo from './img/revivelogo.png'
+
 
 const Landing = () => {
     return (
         <div className="landing">
-            <div><img src="https://images.unsplash.com/photo-1490111718993-d98654ce6cf7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80/316x316" alt="coffee-logo" />
+            <div><img src={revivelogo} alt="coffee-logo" />
             </div>
-            <div className="tc mt3">
-                "Coffee That Matters"
-            </div>
+
         </div>
     )
 }
