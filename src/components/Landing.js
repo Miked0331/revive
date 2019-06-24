21d1d9f21dcaf05aeeecf7b7bc8e621067da37a7
Miked0331 <40614967+Miked0331@users.noreply.github.com>
2019-06-24 17:44:29 -0400

    new logo AGAIN

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
 import revivelogo from './img/revivelogo.png'
+import cover2 from './img/cover2.png'
 
 
 const Landing = () => {
     return (
         <div className="landing">
-            <div><img src={revivelogo} alt="coffee-logo" />
+            <div><img src={cover2} alt="coffee-logo" />
             </div>
 
         </div>
