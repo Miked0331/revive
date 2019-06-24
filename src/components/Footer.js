3a12c78daffb815764e6b0083d81c76e5c9dac52
Miked0331 <40614967+Miked0331@users.noreply.github.com>
2019-06-24 17:35:59 -0400

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,9 @@ const Footer = () => {
             <footer className="pv4 ph3 ph5-m ph6-l mid-gray">
                 <small className="f6 db tc">© 2019 <b class="ttu">REVIVE COFFEE Inc</b>., All Rights Reserved</small>
                 <div className="tc mt3">
-                    <a href="/language/" title="Language" class="f6 dib ph2 link mid-gray dim">Language</a>
-                    <a href="/terms/" title="Terms" class="f6 dib ph2 link mid-gray dim">Terms of Use</a>
-                    <a href="/privacy/" title="Privacy" class="f6 dib ph2 link mid-gray dim">Privacy</a>
+                    <p title="Language" class="f6 dib ph2 link mid-gray dim">Language</p>
+                    <p title="Terms" class="f6 dib ph2 link mid-gray dim">Terms of Use</p>
+                    <p title="Privacy" class="f6 dib ph2 link mid-gray dim">Privacy</p>
                 </div>
             </footer>
         </div>
